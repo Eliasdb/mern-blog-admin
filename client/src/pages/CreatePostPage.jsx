9e46a21e2fd6f1d7743ba7e6232b2c601a7204dd
Elias <elias.db3@gmail.com>
2023-05-17 17:26:23 +0200

    fix header api call

--- client/src/pages/CreatePostPage.jsx
+++ client/src/pages/CreatePostPage.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import Editor from "../components/Editor";
 import { UserContext } from "../../context/UserContext";
